98cf0b38fb4be23733b2fce1ea2b4511afbf89f8
zelalemzola <tzelalemtesfaye@gmail.com>
2024-06-26 14:11:51 +0300

    dsf

--- src/app/page.js
+++ src/app/page.js
@@ -4,10 +4,10 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className='bg-primary w-full h-full '>
-      <div className='flex flex-col items-center justify-center gap-10 pt-40 px-[80px] pb-[250px] '>
-      <h1 className='text-primary text-4xl text-white'>Welcome, Dear Admin choose what you want to control</h1>
+      <div className='flex flex-col items-center justify-center gap-10 pt-40 px-[70px] pb-[250px] '>
+      <h1 className='text-primary text-4xl text-white'>Welcome, Dear Admin Click the button below to go to the Dashbaord</h1>
       <div className='pt-6'>
-          <Link href="/dashboard" className="bg-primary text-white -2 rounded-lg">Go To Dashboard</Link>
+          <Link href="/dashboard" className="bg-primary text-white rounded-lg">Go To Dashboard</Link>
       </div>
       </div>
     </div>
