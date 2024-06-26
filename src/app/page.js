19bc36ef54ac0fffdbab7f39c8c8da014ed3e336
zelalemzola <tzelalemtesfaye@gmail.com>
2024-06-26 14:07:07 +0300

    fsf

--- src/app/page.js
+++ src/app/page.js
@@ -6,15 +6,8 @@ export default function Home() {
     <div className='bg-primary w-full h-full '>
       <div className='flex flex-col items-center justify-center gap-10 pt-40 px-[80px] pb-[250px] '>
       <h1 className='text-primary text-4xl text-white'>Welcome, Dear Admin choose what you want to control</h1>
-      <div className='flex items-center gap-10'>
-        <Link href='/dashboard/categories' className='bg-white text-primary p-2 rounded-lg flex items-center justify-evenly gap-2'>
-        <Blocks/>
-        Categories
-        </Link>
-        <Link href='/dashboard/businesses' className='bg-white text-primary p-2 rounded-lg  flex items-center justify-evenly gap-2'>
-        <BriefcaseBusiness/> 
-          Businesses 
-        </Link>
+      <div className='pt-6'>
+          <Link href="/dashboard" className="bg-primary text-white -2 rounded-lg">Go To Dashboard</Link>
       </div>
       </div>
     </div>
